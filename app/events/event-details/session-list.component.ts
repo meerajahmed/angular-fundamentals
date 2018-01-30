1fb23271014a691afbaa50331db5bc7c42cae93f
meerajahmed <meerajahmed@gmail.com>
2018-01-31 00:22:21 +0530

    sorting data using OnChanges

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,13 +1,32 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
 import {ISession} from "../../shared/event.model";
 
 @Component({
   selector: "session-list",
   templateUrl: "app/events/event-details/session-list.component.html"
 })
-export class SessionLisComponent {
+export class SessionLisComponent implements OnChanges {
+
   @Input()
   sessions: ISession[];
   @Input()
   filterBy: string;
+
+  visibleSessions: ISession[];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if( this.sessions ){
+      this.filterSession(this.filterBy);
+    }
+  }
+
+  filterSession( filter ){
+    if( filter === "all" ){
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLowerCase() === filter;
+      });
+    }
+  }
 }
