e2057d1e007c19378cb5b459c154c5009e90dd8c
meerajahmed <meerajahmed@gmail.com>
2018-02-01 00:14:42 +0530

    sorting data

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -19,6 +19,9 @@ export class SessionLisComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if( this.sessions ){
       this.filterSession(this.filterBy);
+      this.sortBy === "name"
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVote);
     }
   }
 
@@ -31,4 +34,14 @@ export class SessionLisComponent implements OnChanges {
       });
     }
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession){
+  if(s1.name > s2.name) return 1;
+  else if(s1.name === s2.name) return 0;
+  else return -1;
+}
+
+function sortByVote(s1: ISession, s2: ISession){
+  return s2.voters.length - s1.voters.length;
 }
