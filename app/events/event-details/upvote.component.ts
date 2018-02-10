83304e9508d343fae1d9168e35eb80a2ab49a5e1
meerajahmed <meerajahmed@gmail.com>
2018-02-10 23:42:10 +0530

    added voting component stlyes

--- app/events/event-details/upvote.component.ts
+++ app/events/event-details/upvote.component.ts
@@ -12,7 +12,8 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
         <div>{{count}}</div>
       </div>
     </div>
-  `
+  `,
+  styleUrls: []
 })
 export class VotingComponent {
   @Input() count: number;
