396024dc77145bb3755c562f9d60d49b1d138e3d
meerajahmed <meerajahmed@gmail.com>
2018-02-10 23:29:15 +0530

    added voting component

---
+++ app/events/event-details/upvote.component.ts
@@ -0,0 +1,26 @@
+
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+@Component({
+  selector: "upvote",
+  template: `
+    <div class="voting-widget-container pointable" (click)="onclick()" )>
+      <div class="voting-button">
+        <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
+        <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+      </div>
+      <div class="badge badge-inverse voting-count">
+        <div>{{count}}</div>
+      </div>
+    </div>
+  `
+})
+export class VotingComponent {
+  @Input() count: number;
+  @Input() voted: boolean;
+  @Output() vote: EventEmitter<Object> = new EventEmitter();
+
+  onClick() {
+    this.vote.emit({});
+  }
+
+}
