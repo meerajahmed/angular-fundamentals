601cd794d2e80b6d0fe4970b9309021b66b54c83
meerajahmed <meerajahmed@gmail.com>
2018-02-11 22:58:22 +0530

    update property using input setters

--- app/events/event-details/upvote.component.ts
+++ app/events/event-details/upvote.component.ts
@@ -4,12 +4,13 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
   selector: "upvote",
   template: `
     <div class="voting-widget-container pointable" (click)="onClick()">
-      <div class="voting-button">
-        <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-        <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
-      </div>
-      <div class="badge badge-inverse voting-count">
-        <div>{{count}}</div>
+      <div class="well voting-widgetng">
+        <div class="voting-button">
+          <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
+        </div>
+        <div class="badge badge-inverse voting-count">
+          <div>{{count}}</div>
+        </div>
       </div>
     </div>
   `,
@@ -17,8 +18,12 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
 })
 export class VotingComponent {
   @Input() count: number;
-  @Input() voted: boolean;
   @Output() vote: EventEmitter<Object> = new EventEmitter();
+
+  private iconColor: string;
+  @Input() set voted(val: boolean) {
+    this.iconColor = val ? "red" : "white";
+  }
 
   onClick() {
     this.vote.emit({});
