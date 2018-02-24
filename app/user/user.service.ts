fea8eb5aba4892b1e481dabb4c8cddb84011340e
meerajahmed <meerajahmed@gmail.com>
2018-02-24 22:21:07 +0530

    intergrating user auth service

--- app/user/user.service.ts
+++ app/user/user.service.ts
@@ -1,15 +1,28 @@
 import {IUser} from "./user.model";
+import {Http, Headers, RequestOptions, Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/do";
+import "rxjs/add/observable/of";
 
 export class AuthService {
+
   currentUser: IUser;
-  loginUser( userName: string, password: string ): void {
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: "John",
-      lastName: "Papa"
-    }
+
+  constructor(private http: Http){}
+
+  loginUser( userName: string, password: string ): Observable<Response|boolean> {
+    let headers = new Headers({ "Content-Type": "application/json" });
+    let options = new RequestOptions({ headers: headers });
+    let loginInfo = { username: userName, password: password };
+    return this.http.post("/api/login", JSON.stringify(loginInfo), options)
+      .do( res => {
+        if(res){
+          this.currentUser = <IUser>res.json().user;
+        }
+      })
+      .catch(error => Observable.of(false));
   }
+
   isAuthenticated(): boolean{
     return !!this.currentUser;
   }
